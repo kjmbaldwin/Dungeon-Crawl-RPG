589f9172412cd8cd2de627faf7922952c637b236
Kyle Baldwin <kylejmbaldwin@gmail.com>
2017-11-09 22:22:45 -0500

    redesigned the HTML page, all UI elements in place, starting attack button code

--- assets/game.js
+++ assets/game.js
@@ -1,28 +1,29 @@
-// create character objects
-
 var fighter = {
-  hp    : '120',
-  attack: 10,
-  
+  hp      : '120',
+  attack  : 10,
+  counter : 0
 }
 
 var mage = {
-  hp    : '80',
-  attack: 30,
+  hp      : '80',
+  attack  : 30,
+  counter : 0
 }
 
 var theif = {
-  hp    : '100',
-  attack: 5,
+  hp      : '100',
+  attack  : 5,
+  counter : 0
 }
 
 var initialSelection;
 var playerSelect = false;
 var enemySelect = false;
 
-var test = {name: 'kyle', age: '32'}
-$('#test').data(test)
-console.log( $('#test').data());
+// I was testing some stuff here, leaving it in case I need to refer back. 
+// var test = {name: 'kyle', age: '32'}
+// $('#test').data(test)
+// console.log( $('#test').data());
 
 
 
@@ -31,62 +32,63 @@ console.log( $('#test').data());
 // when choose character is slected, move to left side of board 
 // when fight is selected move to right side of board
 
-// function selectCharacter(){
-// $('#fighter').on('click', function(){  
-// $('#fighter').prependTo('#player')
-// });
-// }
 
-// $(this).prependTo('#player')
 
-//trying to find a way to link the opbejcts to the html elements below:
-
-function setStats(){
-  
-  
-  console.log( $(this.id).data('hp'));
-}
-
+//load HTML elements with objects defined above
 $('#fighter').attr(fighter);
 $('#mage').attr(mage);
 $('#theif').attr(theif);
 
+//start with buttons hidden hidden
+$('#attack-btn').hide();
+$('#select-char-btn').hide();
+$('#stats-panel').hide();
+
+
+//print stats in the panel when character image is clicked
 $('.character').on('click', function(){
 
   initialSelection = this;
+  $('#stats-panel').show();
+  $('#select-char-btn').show();
 
   if(!playerSelect){
     $('.stats').html(
-      '<h3>Class: ' + $(this).attr('id') + '</h3>' +
-      '<div>HP: ' + $(this).attr('hp') + '</div>' +
-      '<div>Attack: ' + $(this).attr('attack') + '</div>' 
+      '<p>Class: ' + $(this).attr('id') + '</p>' +
+      '<p>Starting HP: ' + $(this).attr('hp') + '</p>' +
+      '<p>Attack Power: ' + $(this).attr('attack') + '</p>' 
       );
   }
 
   else if (playerSelect && !enemySelect) {
     $('.stats2').html(
-      '<h3>Class: ' + $(this).attr('id') + '</h3>' +
-      '<div>HP: ' + $(this).attr('hp') + '</div>' +
-      '<div>Attack: ' + $(this).attr('attack') + '</div>' 
+      '<p>Class: ' + $(this).attr('id') + '</p>' +
+      '<p>Starting HP: ' + $(this).attr('hp') + '</p>' +
+      '<p>Attack Power: ' + $(this).attr('attack') + '</p>' 
       );
   }
 
-
 });
 
-
+//move character and enemy to game board
 $('#select-char-btn').on('click', function(){
   
   if(!playerSelect){
     playerSelect = true;
     $('#player').append(initialSelection);
-    
+    $('#attack-btn').show();
+    $('#select-char-btn').removeClass('btn-success').addClass('btn-warning').text('Select Enemy');
   }
 
   else if (playerSelect && !enemySelect){
     enemySelect = true;
     $('#enemy').append(initialSelection);
-    $('.stats2').html(""); 
+    // $('.stats2').html(""); 
   }
   
 });
+
+
+/*
+attack button sudo code
+*/
